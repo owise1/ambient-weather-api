afc3035dea58cba7893714b89456ca30434961af
Oliver Wise <owise1@gmail.com>
2018-01-19 11:54:54 -0800

    init commit :partly_sunny:

---
+++ examples/display-recent-data.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const AmbientWeatherApi = require('../lib/index')
+
+const api = new AmbientWeatherApi({
+  apiKey: process.env.AMBIENT_WEATHER_API_KEY || 'Put your AW apiKey here',
+  applicationKey: process.env.AMBIENT_WEATHER_APPLICATION_KEY || 'Put your AW applicationKey here'
+})
+
+// list the user's devices
+api.userDevices()
+.then((devices) => {
+
+  devices.forEach((device) => {
+    // fetch the most recent data
+    api.deviceData(device.macAddress, {
+      limit: 5
+    })
+    .then((deviceData) => {
+      console.log('The 5 most recent temperature reports for ' + device.info.name + ' - ' + device.info.location + ':')
+      deviceData.forEach((data) => {
+        const d = new Date(data.dateutc)
+        console.log(d + ' - ' + data.tempf + '°F')
+      })
+      console.log('---')
+    })
+  })
+})
---
+++ src/index.js
@@ -0,0 +1,48 @@
+import request from 'request-promise'
+import { map, pipe, toPairs, join } from 'ramda'
+
+const AW_API_URL = 'https://ambient-weather-api.herokuapp.com/v1/devices/'
+
+module.exports = class AmbientWeatherApi {
+  constructor (opts) {
+    const { apiKey, applicationKey } = opts
+    if (!apiKey) {
+      throw new Error('You need an apiKey')
+    }
+    if (!applicationKey) {
+      throw new Error('You need an applicationKey')
+    }
+    this.apiKey = apiKey
+    this.applicationKey = applicationKey
+  }
+
+  _apiRequest (url) {
+    return request({
+      url,
+      json: true
+    })
+  }
+
+  _getUrl (macAddress = '') {
+    return AW_API_URL + macAddress + '?apiKey=' + this.apiKey + '&applicationKey=' + this.applicationKey
+  }
+
+  userDevices () {
+    return this._apiRequest(this._getUrl())
+  }
+
+  deviceData (macAddress, opts) {
+    if (!macAddress) {
+      throw new Error('You need a macAddress for deviceData')
+    }
+    let url = this._getUrl(macAddress)
+    if (opts) {
+      url += '&' + pipe(
+        toPairs,
+        map(join('=')),
+        join('&')
+      )(opts)
+    }
+    return this._apiRequest(url)
+  }
+}
